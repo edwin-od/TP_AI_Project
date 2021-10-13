6ab331012cbaa7bd6b2e3112839d9e50e4f8d1c9
Gastonx <56830127+Gastonx@users.noreply.github.com>
2021-10-13 12:04:04 +0200

    Update

--- Assets/Teams/Hugo/HyperionController.cs
+++ Assets/Teams/Hugo/HyperionController.cs
@@ -26,7 +26,14 @@ namespace Hyperion
 		public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
 		{
 			SpaceShipView otherSpaceship = data.GetSpaceShipForOwner(1 - spaceship.Owner);
+
+			behaviourTree.SetVariableValue("EnergyLevel", spaceship.Energy);
+			behaviourTree.SetVariableValue("Score",  (float)spaceship.Score);
+			behaviourTree.SetVariableValue("EnemyPosition",  otherSpaceship.Position);
+			behaviourTree.SetVariableValue("ClosestPoint",data.WayPoints[0].Position);
+			
 			thrust = 1.0f;
+			
 			targetOrient = Orientation;
 			needShoot = AimingHelpers.CanHit(spaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
 			InputData input =new InputData(thrust, targetOrient, needShoot, needMine, needShockwawe);
