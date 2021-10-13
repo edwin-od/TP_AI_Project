101f3b9c28e000cc2ae8575a2d4d5e0e04361ea4
Blue <mathieu.prevot@edu.devinci.fr>
2021-10-13 20:33:15 +0200

    Hyper

--- Assets/Teams/Hugo/TaskGoTo.cs
+++ Assets/Teams/Hugo/TaskGoTo.cs
@@ -10,10 +10,20 @@ public class TaskGoTo : Action
     public BehaviorTree behaviourTree;
     public SharedVector2 target;
     public SharedVector2 Player;
+    public SharedFloat Orientation;
 
-    public override TaskStatus OnUpdate() 
+    public override void OnStart()
     {
-        float rotation = Vector2.SignedAngle(Player.Value, target.Value);
+
+    }
+    public override TaskStatus OnUpdate()
+    {
+
+        //float diff = Vector2.Angle(Player.Value, target.Value);
+        Vector2 diff = target.Value - Player.Value;
+        float rot = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        behaviourTree.SetVariableValue("Rotation", rot);
+
         return TaskStatus.Running;
     }
 
