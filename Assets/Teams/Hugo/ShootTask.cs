86e2c2c6d6100d48733bba974eb852053a979699
Blue <mathieu.prevot@edu.devinci.fr>
2021-10-14 16:27:55 +0200

    controls & shoot & unblockdir

--- Assets/Teams/Hugo/ShootTask.cs
+++ Assets/Teams/Hugo/ShootTask.cs
@@ -11,47 +11,13 @@ public class ShootTask : Action
     private HyperionController hyperion;
     public BehaviorTree behaviourTree;
     public SharedVector2 Player;
-    bool canShoot = false;
     SpaceShipView otherSpaceship;
 
     public override void OnStart()
     {
-        canShoot = false;
         hyperion = gameObject.GetComponent<HyperionController>();
         otherSpaceship = hyperion.currentData.GetSpaceShipForOwner(1 - hyperion.mySpaceship.Owner);
-        Vector2 shipToWaypoint = new Vector2(otherSpaceship.Position.x - Player.Value.x, otherSpaceship.Position.y - Player.Value.y);
-        Debug.DrawRay(Player.Value, shipToWaypoint, Color.yellow);
-        RaycastHit2D[] hits = Physics2D.RaycastAll(Player.Value, shipToWaypoint.normalized, shipToWaypoint.magnitude);
-        for (int k = 0; k < hits.Length; k++)
-        {
-            if (hits[k].collider)
-            {
-                float angle = Mathf.Atan2(shipToWaypoint.y, shipToWaypoint.x) * Mathf.Rad2Deg;
-
-                if (hits[k].transform.tag == "Player" && (Vector2)hits[k].transform.position != hyperion.mySpaceship.Position && hits[k].transform.tag != "Asteroid")
-                {
-                    Debug.Log("can shoot " + hits[k].transform.gameObject.name);
-                    //hyperion.needShoot = AimingHelpers.CanHit(hyperion.mySpaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
-                    canShoot = true;
-
-                }
-
-            }
-        }
-
+        hyperion.needShoot = AimingHelpers.CanHit(hyperion.mySpaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.5f);
     }
-    public override TaskStatus OnUpdate()
-    {
-        Vector2 shipToWaypoint = new Vector2(otherSpaceship.Position.x - Player.Value.x, otherSpaceship.Position.y - Player.Value.y);
-        Debug.DrawRay(Player.Value, shipToWaypoint, Color.yellow);
-        if (canShoot)
-        {
-            behaviourTree.SetVariableValue("TargetPos", otherSpaceship.Position);
-            hyperion.needShoot = AimingHelpers.CanHit(hyperion.mySpaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
-        }
-        //return TaskStatus.Running;
-        if(hyperion.needShoot) return TaskStatus.Success;
-        else{ return TaskStatus.Failure; }
-        //if(hyperion.needShoot) hyperion.needShoot = false;
-    }
+
 }
