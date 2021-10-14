25eba8b2296e0286f0874631bf78a3a88c1ddde7
MarOlaf <karim.ballen@gmail.com>
2021-10-14 19:12:01 +0200

    Final day, Game design 01

---
+++ Assets/Teams/Mathieu/GetHitTime.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using DoNotModify;
+using BehaviorDesigner.Runtime.Tasks;
+
+namespace Hyperion
+{
+    public class GetHitTime : Action
+    {
+        private HyperionController hyperion;
+        SpaceShipView otherSpaceship, currentSpaceship;
+        public SharedVector2 ship;
+
+        public override void OnStart()
+        {
+            hyperion = gameObject.GetComponent<HyperionController>();
+            otherSpaceship = hyperion.currentData.GetSpaceShipForOwner(1 - hyperion.mySpaceship.Owner);
+            if(ship.Name == "PlayerPosition")
+            {
+                currentSpaceship = hyperion.mySpaceship;
+            }
+            else
+            {
+                currentSpaceship = otherSpaceship;
+            }
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            //Debug.Log("hitcount : " + currentSpaceship.HitCount);
+            if (currentSpaceship.HitCount > 0)
+            {
+                return TaskStatus.Success;
+            }
+            else
+            {
+                return TaskStatus.Failure;
+            }
+        }
+    }
+}
